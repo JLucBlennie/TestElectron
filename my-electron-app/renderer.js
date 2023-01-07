7c7b450d1d41fa5dd21c7d5bf35c3717418d9c98
JLucBlennie <jluc.blennie@gmail.com>
2023-01-07 18:08:34 +0100

    Ajout des layer Coogle Maps

--- my-electron-app/renderer.js
+++ my-electron-app/renderer.js
@@ -6,10 +6,36 @@ var osmUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
 osmAttribution = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
 ' <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 osmLayer = new L.TileLayer(osmUrl, { maxZoom: 18, attribution: osmAttribution });
-map.setView([51.505, -0.09], 13);
+map.setView([48.4400912,-4.5214922], 15);
 map.addLayer(osmLayer);
-var marker = new L.marker([51.5, -0.09]).addTo(map);
-marker.bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
+googleSatLayer = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+map.addLayer(googleSatLayer);
+
+googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+var baseMaps = {
+    "OpenStreetMap": osmLayer,
+    "Maps Satellite": googleSatLayer
+};
+
+var layerControl = L.control.layers(baseMaps).addTo(map);
+
+var marker = new L.marker([48.4400912,-4.5214922]).addTo(map);
+marker.bindPopup('Maison<br> Easily customizable.').openPopup();
 
 let information = document.getElementById('info');
 information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`;
